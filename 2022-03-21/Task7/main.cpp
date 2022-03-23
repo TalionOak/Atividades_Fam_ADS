109b4a9d773e2f4cb696a9ee9b5e5b75016c002e
TalionOak <ailtonsscarvalho@gmail.com>
2022-03-23 11:44:40 -0300

    Added Task 7 - not finished yet
    // Atividade 7
    // O custo ao consumidor de um carro novo
    // é a soma do custo de fábrica com a porcetangem do distribuidor e dos impostos
    // Supondo que a porcentagem do distribuidor seja de 28% e o s impostos de 45%
    // leia o custo de fabrica de um carro e escreva o custo ao consumidor.

---
+++ 2022-03-21/Task7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Atividade 7
+// O custo ao consumidor de um carro novo
+// é a soma do custo de fábrica com a porcetangem do distribuidor e dos impostos
+// Supondo que a porcentagem do distribuidor seja de 28% e o s impostos de 45%
+// leia o custo de fabrica de um carro e escreva o custo ao consumidor.
+
+int main()
+{
+    cout << "Não entendi como resolver essa, é pra tirar uma porcentagem do custo de fabrica, ou aumentar o preço?" << endl;
+    return 0;
+}
