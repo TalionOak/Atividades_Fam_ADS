5a9fb51d4d15832ec31e7f3c4bc8fc36385fa8c6
TalionOak <ailtonsscarvalho@gmail.com>
2022-03-23 11:01:20 -0300

    Added Task 5
    // Atividade 5
    // Leia o tempo de duração de um evento expresso em segundos
    // e mostre expresso em horas, minutos e segundos.

---
+++ 2022-03-21/Task5/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <locale>
+#include <math.h>
+
+using namespace std;
+
+// Atividade 5
+// Leia o tempo de duração de um evento expresso em segundos
+// e mostre expresso em horas, minutos e segundos.
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int segundosInformado, minutos, horas, segundos;
+
+    cout << "Por favor, informe um tempo em segundos: ";
+    cin >> segundosInformado;
+
+    horas = segundosInformado / 3600;
+    minutos = (segundosInformado / 60) % 60;
+    segundos = ( segundosInformado) % 60;
+
+    cout << "O tempo de duração será de: " << horas << "h " << minutos << "m " << segundos << "s..." << endl << endl;
+
+    return 0;
+}
